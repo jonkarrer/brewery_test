69252150e34395af42cd9ec8bf9cb81ca5fbc718
jonkarrer <karrerjon@gmail.com>
2021-12-13 14:59:12 -0600

    feat: log all breweries from api, display all

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import type { IBrewery } from '../utils/interface';
 import './styles/App.css';
 
-interface AppProps {}
+const App: FC = () => {
+  const [allBrews, setAllBrews] = useState([] as Array<IBrewery>);
 
-function App({}: AppProps) {
+  async function getBreweries() {
+    const req = await fetch('https://api.openbrewerydb.org/breweries');
+    const data = await req.json();
+
+    setAllBrews(data);
+
+    console.log(data);
+  }
+  useEffect(() => {
+    getBreweries();
+  }, []);
   return (
     <div className="App-container">
       <h1>My Brew</h1>
+      {allBrews.map((item: IBrewery) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ utils/interface.ts
@@ -0,0 +1,18 @@
+export interface IBrewery {
+  id: string;
+  latitude: string | null;
+  longitude: string | null;
+  name: string;
+  phone: string;
+  postal_code: string;
+  state: string;
+  street: string | null;
+  country: string;
+  updated_at: string;
+  website_url: string | null;
+  brewery_type: string | null;
+  created_at: string | null;
+  county_province: null;
+  address_2: null;
+  address_3: null;
+}
